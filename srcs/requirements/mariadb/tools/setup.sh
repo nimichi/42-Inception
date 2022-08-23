76b7347b7b1a615f1ae5c620e33dac0ebe132c2e
Michael Nies <mnies@1-C-22.42heilbronn.de>
2022-08-23 21:28:48 +0200

    not working yet

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-/etc/init.d/mariadb setup
-/etc/init.d/mariadb start
-mysql_secure_installation <<_EOF_
+mysql_install_db --user=$MYSQL_USER --datadir=/var/lib/mysql
+mysql_secure_installation
 
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-n
-Y
-Y
-_EOF_
+sleep 2000
 
-echo "CREATE DATABASE IF NOT EXISTS WORDPRESSDATABASE;" | mysql -u root
-echo "GRANT ALL ON WORDPRESSDATABASE.* TO '$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+# /etc/init.d/mariadb setup
+# /etc/init.d/mariadb start
+# mysql_secure_installation <<_EOF_
 
-exec /usr/bin/mysqld --user=mysql --console
+# Y
+# $MYSQL_ROOT_PASSWORD
+# $MYSQL_ROOT_PASSWORD
+# Y
+# n
+# Y
+# Y
+# _EOF_
+
+# echo "CREATE DATABASE IF NOT EXISTS WORDPRESSDATABASE;" | mysql -u root
+# echo "GRANT ALL ON WORDPRESSDATABASE.* TO '$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+
+# exec /usr/bin/mysqld --user=$MYSQL_USER --console
